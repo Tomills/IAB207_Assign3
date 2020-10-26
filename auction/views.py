44e9c0ffc9695be3b96e9301bb7f9ab78a4f2552
Tomills <mills25t@gmail.com>
2020-10-26 11:51:48 +1000

    Implemented dynamic display of a record card on index page

--- auction/views.py
+++ auction/views.py
@@ -1,8 +1,10 @@
 from flask import Blueprint, render_template
+from .models import Item
 
 bp = Blueprint('main', __name__)
 
 
 @bp.route('/')
 def index():
-    return render_template('index.html')
+    items = Item.query.all()
+    return render_template('index.html', items=items)
