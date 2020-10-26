3a2f7a5b62d2d78798787dd3e5c1a5b94d6f5444
Tomills <mills25t@gmail.com>
2020-10-26 18:05:58 +1000

    Added functionality to display 6 most recent items according to date posted

--- auction/views.py
+++ auction/views.py
@@ -1,9 +1,12 @@
 from flask import Blueprint, render_template
 from .models import Item
+from datetime import date
 bp = Blueprint('main', __name__)
 
 
 @bp.route('/')
 def index():
+    today = date.today()
     items = Item.query.all()
-    return render_template('index.html', items=items)
+    recentitems = Item.query.order_by(Item.date_posted).limit(6)
+    return render_template('index.html', items=items, recentitems=recentitems)
