2a9f3a5bee60a5da7b85227bab48c1c08a8777ad
Tomills <mills25t@gmail.com>
2020-10-27 10:08:44 +1000

    Added dynamic display of 'all genre' section cards on index page

--- auction/views.py
+++ auction/views.py
@@ -1,12 +1,10 @@
 from flask import Blueprint, render_template
 from .models import Item
-from datetime import date
 bp = Blueprint('main', __name__)
 
 
 @bp.route('/')
 def index():
-    today = date.today()
     items = Item.query.all()
     recentitems = Item.query.order_by(Item.date_posted).limit(6)
     return render_template('index.html', items=items, recentitems=recentitems)
