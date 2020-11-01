79aa65bd5963307af4eb81587cb1332189336d8f
sarahdixon98 <sl.dixon@connect.qut.edu.au>
2020-11-01 20:23:32 +1000

    Sarah's Changes

--- auction/views.py
+++ auction/views.py
@@ -8,9 +8,9 @@ bp = Blueprint('main', __name__)
 @bp.route('/')
 def index():
     user_id = current_user.get_id()
-    items = Item.query.all()
+    items = Item.query.with_entities(Item.id, Item.name, Item.description, Item.artist, Item.genre, Item.year, Item.designation, Item.image, Item.starting_value, Item.current_value, Item.bid_number, Item.status, Item.user_id, Item.date_posted, Watchlist.id.label("watchlist_id")).outerjoin(Watchlist).filter(or_(Watchlist.user_id==user_id, Watchlist.user_id == None)).all()
     recentitems = Item.query.with_entities(Item.id, Item.name, Item.description, Item.artist, Item.genre, Item.year, Item.designation, Item.image, Item.starting_value, Item.current_value, Item.bid_number, Item.status, Item.user_id, Item.date_posted, Watchlist.id.label("watchlist_id")).outerjoin(Watchlist).filter(or_(Watchlist.user_id==user_id, Watchlist.user_id == None)).order_by(Item.date_posted.desc()).limit(6)
-    popularitems = Item.query.order_by(Item.bid_number.desc()).limit(6)
+    popularitems = Item.query.with_entities(Item.id, Item.name, Item.description, Item.artist, Item.genre, Item.year, Item.designation, Item.image, Item.starting_value, Item.current_value, Item.bid_number, Item.status, Item.user_id, Item.date_posted, Watchlist.id.label("watchlist_id")).outerjoin(Watchlist).filter(or_(Watchlist.user_id==user_id, Watchlist.user_id == None)).order_by(Item.bid_number.desc()).limit(6)
     return render_template('index.html', items=items, recentitems=recentitems, popularitems=popularitems)
 
 
